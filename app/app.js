dd1abb6f5ef1679b9dd7dc75d015551fbb61d140
Moeen Mahmud <write2fahim007@gmail.com>
2021-09-05 18:56:36 +0600

    refactoring #2

--- app/app.js
+++ app/app.js
@@ -1,6 +1,9 @@
+/**
+ *
+ */
 window.addEventListener("keydown", (e) => {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+  const key = document.querySelector(`.key[data-key="${e.key}"]`);
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
